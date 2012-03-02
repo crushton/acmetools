98310d81c419c711a4e7b24fa4c129cda5831c1c
Chris Rushton <myst3k@gmail.com>
2012-03-02 00:06:41 -0500

    added license and updated path to use env

--- 9200collect.pl
+++ 9200collect.pl
@@ -1,7 +1,22 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
+###
+# Copyright (c) 2012, Chris Rushton
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+##
 
-## This script ftp's the log files and crash dumps from a 9200 to the local machine.
-## It puts the files in a local directory of the hostname.
+# This script ftp's the log files and crash dumps from a 9200 to the local machine.
+# It puts the files in a local directory of the hostname.
 
 use Net::FTP;
 use File::Path;
--- convert262.pl
+++ convert262.pl
@@ -1,9 +1,25 @@
-#!/usr/bin/perl
-## This script converts HMR's on a 4250 running 6.0/6.1 to the new 6.2 format.
-## All hyphens are converted to underscores in header rule name and element rule name.
-## All uppercase letters are converted to lowercase letters in header rule name and element rule name.
-## All header rule or element rule names starting with a number have a "z" appened to the front.
-## All changes to header rule name and element rule names are updated in match value and new value in case they are used as variables.
+#!/usr/bin/env perl
+###
+## Copyright (c) 2012, Chris Rushton
+##
+## Permission to use, copy, modify, and distribute this software for any
+## purpose with or without fee is hereby granted, provided that the above
+## copyright notice and this permission notice appear in all copies.
+##
+## THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+## WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+## MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+## ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+## WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+## ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+## OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+###
+#
+# This script converts HMR's on a 4250 running 6.0/6.1 to the new 6.2 format.
+# All hyphens are converted to underscores in header rule name and element rule name.
+# All uppercase letters are converted to lowercase letters in header rule name and element rule name.
+# All header rule or element rule names starting with a number have a "z" appened to the front.
+# All changes to header rule name and element rule names are updated in match value and new value in case they are used as variables.
 
 ## Requrements
 # gzip
