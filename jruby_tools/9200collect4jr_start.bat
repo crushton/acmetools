4eb50f281c38325ab9173373b3af56eaed0c2a4c
Chris Rushton <myst3k@gmail.com>
2012-10-24 19:25:55 -0400

    fixed bat file to work

--- jruby_tools/9200collect4jr_start.bat
+++ jruby_tools/9200collect4jr_start.bat
@@ -1,10 +1,6 @@
 @echo off
 
-set GEM_PATH="gems"
-set GEM_HOME="gems"
-set JRUBY_COMPLETE="jar/jruby-complete-1.7.0.jar"
-set JRUBY="java -Xmx500m -Xss1024k -jar %JRUBY_COMPLETE%"
+set GEM_PATH=gems
+set GEM_HOME=gems
 
-set SCRIPT="ruby/9200collect4jr.rb"
-
-%JRUBY% %SCRIPT% %1 %2 %3 %4 %5 %6 %7 %8 %9%
+java -Xmx500m -Xss1024k -jar "jar\jruby-complete-1.7.0.jar" "ruby\9200collect4jr.rb" %1 %2 %3 %4 %5 %6 %7 %8 %9
